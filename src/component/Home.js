5f0b109746c325b9df63650bb5873212ab5f825f
reem <janinareem@gmail.com>
2022-06-29 18:51:27 +0200

    Update Home compoennt

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,9 +1,11 @@
 import Header from './Header';
+import Works from './Works';
 import '../styles/Home.css';
 
 const Home = () => (
   <div className="wrap">
     <Header />
+    <Works />
   </div>
 );
 
