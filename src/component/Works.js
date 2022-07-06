a9edfd0e945188ce08a031cbe5c7426af07e5a80
reem <janinareem@gmail.com>
2022-07-06 22:55:23 +0200

    Add a link to works section

--- src/component/Works.js
+++ src/component/Works.js
@@ -1,7 +1,10 @@
+// import { useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import '../styles/Works.css';
 import projects from '../images/project7.jpg';
 
 const Works = () => {
+  // const history = useHistory();
   return (
     <div className="works">
         <div className="side-text">
@@ -10,9 +13,12 @@ const Works = () => {
               <div className="horizontal-line-red" />
                <div className="horizontal-line-red second" />
            </div>
-         <button className="btn">
+         {/* <button className="btn" onClick={() => history.push('/Projects')}>
             Show me more
-           </button>
+           </button> */}
+            <Link to="/Projects" className="btn" exact>
+              Show me more
+            </Link>
         </div>
         <div className="img-div">
            <img className="image" src={projects} alt="projects" />
