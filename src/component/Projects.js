164f8d3946fc4a00e7ebca2c561908825d679887
reem <janinareem@gmail.com>
2022-07-07 21:52:01 +0200

    Add the header section for projects

--- src/component/Projects.js
+++ src/component/Projects.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
+import projects from '../images/project7.jpg';
+import { FaArrowLeft } from 'react-icons/fa';
+import '../styles/Projects.css'
 
-export default function Projects() {
+const Projects = () => {
   return (
-    <div>Projects</div>
+    <section className="projects">
+     <div className="works">
+     <Link className="arrow" to="/"> <FaArrowLeft className="backArrow" /></Link>
+      <div className="side--text-projects">
+        <p className="projects--text">MOST HIGHLIGHTED PORJECTS</p>
+          <div className="lines">
+              <div className="horizontal--line-red" />
+               <div className="horizontal-line-red second" />
+           </div>
+      </div>
+      <div className="img-div">
+           <img className="image2" src={projects} alt="projects" />
+        </div>
+        </div>
+    </section>
   )
 }
+
+export default Projects;
