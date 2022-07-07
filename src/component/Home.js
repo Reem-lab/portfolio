36cf5a619bba6e9e10f04e911fd766ba2ca8871e
reem <janinareem@gmail.com>
2022-07-07 21:51:41 +0200

    Add the navbar component

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,9 +1,11 @@
 import Header from './Header';
 import Works from './Works';
+import Navbar from './Navbar';
 import '../styles/Home.css';
 
 const Home = () => (
   <div className="wrap">
+    <Navbar />
     <Header />
     <Works />
   </div>
