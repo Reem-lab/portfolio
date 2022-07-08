038db1229149967fe7fb76f10e022f92da90a2df
reem <janinareem@gmail.com>
2022-07-09 00:03:26 +0200

    Add the cards for the projects

---
+++ src/component/Cards.js
@@ -0,0 +1,112 @@
+
+import '../styles/Cards.css';
+import rent from '../images/rentmedesktop.PNG';
+import covid from '../images/covidproject.PNG';
+import space from '../images/spacetraveler.PNG';
+import recipe from '../images/recipe.PNG';
+import pokemon from '../images/pokemons.PNG';
+import budget from '../images/budgetapp.PNG';
+import book from '../images/bookstore.PNG';
+import mapty from '../images/mapty.PNG';
+import unicef from '../images/unicef.PNG';
+
+const Cards = () => {
+
+const Data = [
+  {
+   image: rent,
+   name: "Rent Me",
+   liveLink: "https://rent-me-capstone-project.netlify.app/login",
+   sourceLink: "https://github.com/Reem-lab/house-booking-back-end",
+   description: "This Project is based on an app to book an appointment to rent a house, You can see different types of houses and you can see the details for each house then you can reserve an appointment to see it, you can see all your reservations on a separate page.",
+  },
+  {
+    image: covid,
+    name: "Covid19-Tracker",
+    liveLink: "https://covid19-tracker9.netlify.app/",
+    sourceLink: "https://github.com/Reem-lab/covid-tracker",
+    description: "This project is talking about covid cases in all countries in the world and also displays all the information details to each country with all covid cases or recovered cases.",
+   },
+   {
+    image: space,
+    name: "Space Travellers' Hup",
+    liveLink: "https://62201e415f9b8d1761cf3e80--space-travels-hub.netlify.app/",
+    sourceLink: "https://github.com/Reem-lab/space-travelers-hub",
+    description: "Web application that allows users to book rockets and join selected space missions using real live data from the SpaceX API",
+   },
+   {
+    image: recipe,
+    name: "Recipe App",
+    liveLink: "https://powerful-mountain-66036.herokuapp.com/",
+    sourceLink: "https://github.com/Reem-lab/Recipe-app",
+    description: "The Recipe app keeps track of all your recipes, ingredients, and inventory. It will allow you to save ingredients, keep track of what you have, create recipes, and generate a shopping list based on what you have and what you are missing from a recipe.",
+   },
+   {
+    image: pokemon,
+    name: "Pokemon API",
+    liveLink: "https://nicolaswg.github.io/Capstone_Pokemon_Api/",
+    sourceLink: "https://github.com/Reem-lab/Capstone_Pokemon_Api",
+    description: "This project is JavaScript web application The topic about POKEMONS so we get pokemons from API and display it with the ability to Add your insights in any pokemon you want.",
+   },
+   {
+    image: budget,
+    name: "Budget App",
+    liveLink: "https://agile-atoll-37898.herokuapp.com/",
+    sourceLink: "https://github.com/Reem-lab/budget-app",
+    description: "Budget app is about building a mobile web application where you can manage your budget: you have a list of transactions associated with a category, so that you can see how much money you spent and on what",
+   },
+   {
+    image: unicef,
+    name: "Unicefs'conference",
+    liveLink: "https://reem-lab.github.io/CapstoneProject/",
+    sourceLink: "https://github.com/Reem-lab/CapstoneProject",
+    description: "This project is Myfirst Capstone project Like real world Projects, It is about a conference for charitable organization to help needy people, orphans and refugees made with html,css and javascript",
+   },
+   {
+    image: book,
+    name: "Book store",
+    liveLink: "https://book-store-reactjs.netlify.app/",
+    sourceLink: "https://github.com/Reem-lab/book-store-reactjs",
+    description: "This is a simple react project that Stores the books, When the user enter any book with the category will be updated in the API and you can get someof books from API based on the category.",
+   },
+   {
+    image: mapty,
+    name: "Mapty",
+    liveLink: "https://maptyapplic.netlify.app/",
+    sourceLink: "https://github.com/Reem-lab/Mapty-App",
+    description: "This Application made with vanilla JavaScript, This app contain a map and you can press on anywhere then specify what you want to do there running or cycling",
+   },
+]
+
+  return (
+    <>
+    <div className="header-works">
+    <h2 className="heading-two">MY WORK</h2>
+      <p className="summary">
+      A small gallery of recent projects chosen by me.
+      For more projects, please <a className="link" href="https://github.com/Reem-lab">visit</a> my Github
+      </p>
+    </div>
+    <div className="card-container">
+        { Data.map((card) => (
+             <div className="card">
+              <div className="face face1">
+              <img src={card.image} alt="project1" className="image-project" />
+              <p className="project-name">{card.name}</p>
+              </div>
+              <div className="face face2">
+                  <p className="description">{card.description}</p>
+                  <div className="links-div">
+                  <a className="link" href={card.liveLink}>Go live</a>
+                  <a  className="link"href={card.sourceLink}>source code</a>
+                  </div>
+              </div>
+              </div>
+          ))
+        }
+    </div>
+    </>
+  )
+}
+
+export default Cards;
