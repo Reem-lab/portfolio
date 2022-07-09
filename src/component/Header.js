8eca18b585fe7cfe93f7460c044e9aa2017358e3
reem <janinareem@gmail.com>
2022-07-09 03:44:49 +0200

    Add the animation for the cards

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,3 +1,4 @@
+import { Animate } from 'react-simple-animate';
 import '../styles/Header.css';
 import Cloud2 from './cloud2';
 import Cloud3 from './cloud3';
@@ -12,6 +13,17 @@ const Header = () => (
       <div className="horizontal-line" />
     </div>
     <div className="info">
+    <Animate
+  play
+  duration={1.5}
+  delay={1}
+  start={{
+    transform: "translateX(-1000px)",
+  }}
+  end={{
+    transform: "translatex(0px)",
+  }}
+  >
       <h1 className="name">REEM</h1>
       <h2 className="last-name">JANINA</h2>
       <div className="lines">
@@ -19,12 +31,25 @@ const Header = () => (
         <div className="horizontal-line-red second" />
       </div>
       <h3 className="job-title">Full-stack web developer</h3>
-    </div>
+      </Animate>
+      </div>
     <div className="moon-clouds">
+    <Animate
+  play
+  duration={1.5}
+  delay={1}
+  start={{
+    transform: "translateX(1000px)",
+  }}
+  end={{
+    transform: "translatex(0px)",
+  }}
+  >
       <Cloud2 />
       <img className="moon" src={img6} alt="moon" />
       <Cloud3 />
       <p className="porfolio">PORTFOLIO</p>
+      </Animate>
     </div>
   </div>
 );
