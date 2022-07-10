dea0f42d6e39148cf56e357f505a0a4b199a02af
reem <janinareem@gmail.com>
2022-07-10 22:21:13 +0200

    Add information about me

--- src/component/InfoMe.jsx
+++ src/component/InfoMe.jsx
@@ -1,7 +1,8 @@
 import { Link } from 'react-router-dom';
 import about from '../images/about.jpg';
 import { FaArrowLeft } from 'react-icons/fa';
-import '../styles/Projects.css'
+import '../styles/InfoMe.css';
+import WhoMe from './WhoMe';
 
 
 const InfoMe = () => {
@@ -10,18 +11,20 @@ const InfoMe = () => {
     <section className="projects">
      <div className="works-projects">
      <Link className="arrow" to="/"> <FaArrowLeft className="backArrow" /></Link>
-      <div className="side--text-projects">
-        <p className="projects--text">About ME</p>
+      <div className="side--text-about">
+        <p className="info-projects--text">About ME</p>
           <div className="lines">
               <div className="horizontal--line-red" />
                <div className="horizontal-line-red second" />
            </div>
+           <span className="span-text">I love Design, Technology, and React.</span>
       </div>
       <div className="img-div">
            <img className="image2" src={about} alt="about" />
         </div>
         </div>
     </section>
+    <WhoMe />
      </>
   )
 }
