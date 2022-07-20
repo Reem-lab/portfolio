5184122ef8a6a399c321ad0c341c11380708fbef
reem <janinareem@gmail.com>
2022-07-20 17:56:52 +0200

    Update the name convension and Fix linters

---
+++ src/component/Works.js
@@ -0,0 +1,52 @@
+// import { useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { Animate } from 'react-simple-animate';
+import '../styles/Works.css';
+import projects from '../images/project7.jpg';
+
+const Works = () => (
+  <div id="works" className="works">
+    <div className="side-text">
+      <Animate
+        play
+        duration={1.5}
+        delay={1}
+        start={{
+          transform: 'translateX(-1000px)',
+        }}
+        end={{
+          transform: 'translatex(0px)',
+        }}
+      >
+        <p className="projects-text">MOST HIGHLIGHTED PROJECTS</p>
+        <div className="lines lines-works">
+          <div className="horizontal-line-red" />
+          <div className="horizontal-line-red second" />
+        </div>
+        <Link to="/Projects" className="btn show-more" exact>
+          Show me more
+        </Link>
+      </Animate>
+    </div>
+    <div className="img-div">
+      <Animate
+        play
+        duration={1.5}
+        delay={1}
+        start={{
+          transform: 'translateX(1000px)',
+          scrollBehavior: 'smooth',
+        }}
+        end={{
+          transform: 'translatex(0px)',
+        }}
+      >
+        <img className="image" src={projects} alt="projects" />
+        <p className="number-text">01</p>
+      </Animate>
+    </div>
+
+  </div>
+);
+
+export default Works;
