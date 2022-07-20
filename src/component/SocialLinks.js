946386a26e7faab7f0164e3ed0aaa96f20ccad67
reem <janinareem@gmail.com>
2022-07-20 17:56:39 +0200

    Update the name convension and Fix linters

--- src/component/SocialLinks.js
+++ src/component/SocialLinks.js
@@ -2,23 +2,21 @@ import { AiOutlineTwitter, AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
 import { FaAngellist } from 'react-icons/fa';
 import '../styles/SocialLinks.css';
 
-const SocialLinks = () => {
-  return (
-    <div className="footer social-links--site">
+const SocialLinks = () => (
+  <div className="footer social-links--site">
     <a className="anchor-link" href="https://twitter.com/Rem79940127">
       <AiOutlineTwitter className="social-icon--footer" />
     </a>
-    <a className="anchor-link" href='https://github.com/Reem-lab'>
+    <a className="anchor-link" href="https://github.com/Reem-lab">
       <AiFillGithub className="social-icon--footer" />
     </a>
-    <a className="anchor-link" href="https://www.linkedin.com/in/reemjanina/" >
+    <a className="anchor-link" href="https://www.linkedin.com/in/reemjanina/">
       <AiFillLinkedin className="social-icon--footer" />
     </a>
-    <a className="anchor-link" href="https://angel.co/u/reem-janina" >
+    <a className="anchor-link" href="https://angel.co/u/reem-janina">
       <FaAngellist className="social-icon--footer" />
     </a>
   </div>
-  )
-}
+);
 
 export default SocialLinks;
